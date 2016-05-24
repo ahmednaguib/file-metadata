8537cd5423ed8f117ddb8ac6fcb0c34f3682d1a6
Naguib <ahmed.naguib@live.com>
2016-05-24 21:39:34 +0000

    change homepage to add description

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,6 +7,11 @@ module.exports = function (app, db) {
 	app.route('/r/:id')
 	.get(apiController.redirectUrl);
 
+	app.route('/')
+	.get(function(req,res) {
+		res.sendFile(path + '/public/index.html');
+	});
+	
 	app.route('/*')
 	.get(apiController.shortenUrl);
 
