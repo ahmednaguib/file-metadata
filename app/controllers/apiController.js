bb2f69c16291c2104017e2042222f6011bfc3bf1
Naguib <ahmed.naguib@live.com>
2016-05-29 11:32:29 +0000

    finalize the search call

--- app/controllers/apiController.js
+++ app/controllers/apiController.js
@@ -4,7 +4,25 @@ var searchItem = require('../models/searchItem');
 var googleSearch = require('../services/googleSearchApi');
 
 exports.imagesearch = function(req, res, next) {
+    
+    var printResponse = function(result){
+     if (result instanceof Error) {
+        console.log('Error:', result.message);
+      } else {
+        var items = result.items;
+        var responseArray = [];
+        items.forEach(function(item) {
+            responseArray.push({
+            'title': item.title, 
+            'url': item.link, 
+            'thumbnail': item.image.thumbnailLink,
+            'context': item.image.contextLink
+            });
+        });
+        res.json(responseArray);
+      }
+    };
+    
     var keyword = req.params.keyword;
-    var results = googleSearch.searchWithKeyWord(keyword);
-    res.json(results);
+    googleSearch.searchWithKeyWord(keyword, printResponse);
 };
--- app/services/googleSearchApi.js
+++ app/services/googleSearchApi.js
@@ -1,23 +1,18 @@
 'use strict';
- var rest = require('restler');
 
-exports.searchWithKeyWord = function(keyword){
+var rest = require('restler');
+
+exports.searchWithKeyWord = function(keyword ,callback){
     if(!keyword){
-        return [];
+        callback([]);
+        return;
     }
-    var apiKey = process.argv.GOOGLE_API_KEY;
-    var baseUrl = process.argv.GOOGLE_BASE_API_URL;
+    var apiKey = process.env.GOOGLE_API_KEY;
+    var baseUrl = process.env.GOOGLE_BASE_API_URL;
     var url = baseUrl + "?safe=medium&searchType=image&key="+ apiKey+"&q=" + keyword;
-    
-    rest.get(url).on('complete', function(result) {
-        
-      if (result instanceof Error) {
-        console.log('Error:', result.message);
-        this.retry(2000); // try again after 5 sec
-      } else {
-        console.log(result);
-        return result;
-      }
+    rest.get(url).on('complete', function(data) {
+      callback(data);
+      return;
     });
 };
 
